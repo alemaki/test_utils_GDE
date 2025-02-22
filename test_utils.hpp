870a5f0528f23109730822f20c9b75e4476186af
Alexander Makedonski <alemak2002@gmail.com>
2025-02-22 12:46:37 +0200

    Added get_all_children for nodes.

--- test_utils.hpp
+++ test_utils.hpp
@@ -12,4 +12,23 @@ void simulate(godot::Node* node, int frames = 1);
 double get_node_delta(godot::Node* node);
 double get_current_engine_delta();
 
+template<typename T>
+godot::TypedArray<T> get_all_children_of_type(godot::Node* node)
+{
+    static_assert(std::is_base_of<godot::Node, T>::value, "T must inherit from godot::Node");
+    ERR_FAIL_NULL_V(node, {});
+    godot::TypedArray<godot::Node> children = node->get_children();
+    godot::TypedArray<T> result;
+    for (int i = 0, size = children.size(); i < size; i++)
+    {
+        godot::Node* child = godot::Object::cast_to<godot::Node>(children[i]);
+        if (child->get_class() == T::get_class())
+        {
+            result.push_back(child);
+        }
+        result.append_array(get_all_children_of_type<T>(child));
+    }
+    return result;
+}
+
 #endif /* BT_TEST_UTILS_HPP */
